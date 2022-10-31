0386d0bd9688f76790bc7dea88cf2667a42d7052
Javier Cervera <84129491+Jacercen@users.noreply.github.com>
2022-10-31 13:24:43 +0100

    Add files via upload

---
+++ ej2/ej2.js
@@ -0,0 +1,16 @@
+const name = "Javier";
+const edad = 25;
+const des = true;
+const fecha = new Date(1996,10,26);
+const libro ={
+    titulo: "Amanecer Rojo",
+    autor: "Pierce Brown",
+    fecha: new Date(2014, 9, 2),
+    url: "https://www.amazon.es/Amanecer-FICCI%C3%93N-PIERCE-ELLIOTT-BROWN/dp/8427208383"
+}
+
+console.log(name);
+console.log(edad);
+console.log(des);
+console.log(fecha);
+console.log(libro);
