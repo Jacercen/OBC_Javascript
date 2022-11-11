b8013f53aef565a489dca26256062b3b003199d2
Javier Cervera <84129491+Jacercen@users.noreply.github.com>
2022-11-11 13:34:03 +0100

    Add files via upload

---
+++ ej5/ej5.js
@@ -0,0 +1,22 @@
+// ej 5
+
+let alt_cm = 181;
+console.log(alt_cm);
+
+let alt_m = 1.81;
+console.log(alt_m);
+
+let peso_kg = 79.7
+console.log(peso_kg);
+
+let altura = Math.ceil(alt_m);
+console.log(altura);
+
+let peso = Math.floor(peso_kg);
+console.log(peso);
+
+let maxnumber = Number.MAX_VALUE === Number.MAX_VALUE+1
+
+console.log(maxnumber);
+
+
